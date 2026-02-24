72ce59c3178b7d330cb95e52734da74fb9fb4fa9
Amol Rairikar <arairikar1@gmail.com>
2026-02-23 21:37:56 -0600

    fix: updating lambda policy

--- infrastructure/aws/main.tf
+++ infrastructure/aws/main.tf
@@ -303,7 +303,8 @@ data "aws_iam_policy_document" "gtfs_expected_schedule_lambda_policy_document" {
     effect = "Allow"
 
     actions = [
-      "s3:PutObject"
+      "s3:PutObject",
+      "s3:GetObject"
     ]
 
     resources = [
